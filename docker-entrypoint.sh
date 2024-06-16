8daee39f6ec781fd9ffde0b85840ef46ef897fdf
huangxun201314 <huangxun2012@126.com>
2024-06-17 00:48:59 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,6 +5,8 @@ if [ ! -f index.php ]; then
     git clone --depth 1 -q ${REPO_URL} .
     rm -rf .git*
     chmod a+rw -R application runtime upload static addons
+    unzip ./template/cmsV10.zip -d ./template
+    rm -rf ./template/cmsV10.zip
     echo "maccms10 downloaded"
 fi
 
